aee2f02426830f61b1205d1735e06c158fd77f4d
Elizabethweatherby <elizabethwarren2792@gmail.com>
2023-08-23 15:43:32 -0500

    removed connections

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "bluedog",
-    database: "schoolEmployee_db",
-});
 
 connection.connect
 begin();
